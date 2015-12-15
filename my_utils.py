69cd1699601676a9e663c61997c5670f353b8e5e
Sebastien <sdubois.sebastien@gmail.com>
2015-12-15 14:58:48 +0100

    refacto

---
+++ my_utils.py
@@ -0,0 +1,76 @@
+from artist_network import *
+from song_db import *
+from config import *
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+import pickle
+
+import pyen
+import spotipy
+import spotipy.util as util
+
+def login(scope='user-library-read'):
+    token = util.prompt_for_user_token(username, scope)    
+    return(token)
+
+def save_likes(liked_artists):
+	output_file = open('liked_artists.csv', 'wb')
+	pickle.dump(liked_artists,output_file)
+	output_file.close()
+
+def load_likes():
+	input_file = open('liked_artists.csv', 'r')
+	liked_artists = pickle.load(input_file)
+	input_file.close()
+
+	return liked_artists
+
+def add_to_history(item,history):
+	title = item['track']['name']
+	id = 'spotify:artist:' + item['track']['artists'][0]['id']
+	if not(id in history):
+		history[id] = []
+	history[id].append(title.lower())
+
+
+def get_liked_tracks(p_name='Liked',history={}):
+	token = login()
+
+	if token:
+		sp = spotipy.Spotify(auth=token)
+		playlists = sp.user_playlists(username)['items']
+		playlist = None
+		for p in playlists:
+			if(p['name'] == p_name):
+				playlist = p
+
+		liked_tracks = set()
+		liked_artists = set()
+		if(playlist is not None):
+			results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+			tracks = results['tracks']
+			liked_tracks.update([ item['track']['name'] for item in tracks['items'] ])
+			liked_artists.update([ item['track']['artists'][0]['name'] for item in tracks['items'] ])
+			map(lambda item: add_to_history(item, history=history),tracks['items'])
+
+			while tracks['next']:
+				print('go to next')
+				liked_tracks.update([ item['track']['name'] for item in tracks['items'] ])
+				liked_artists.update([ item['track']['artists'][0]['name'] for item in tracks['items'] ])
+				map(lambda item: add_to_history(item, history=history),tracks['items'])
+				tracks = sp.next(tracks)
+
+			save_likes(liked_artists)
+
+			output_file = open('history.csv', 'wb')
+			pickle.dump(history,output_file)
+			output_file.close()
+			return history
+		else:
+			print('Error, cannot find playlist')
+
+	else:
+		print("Can't get token for", username)
---
+++ song_db.py
@@ -0,0 +1,26 @@
+import pandas as pd
+limit_per_artist = 20
+
+def populate_db(artists,en,reset=False):
+	df = pd.DataFrame()
+	for artist in artists:
+		print('search',artist)
+		response = en.get('song/search', artist_id =artist, results=limit_per_artist,
+						  bucket=['song_hotttnesss','id:spotify','tracks','audio_summary'], sort='song_hotttnesss-desc')['songs']
+		for song in response:
+			if(len(song['tracks']) > 0 and 'foreign_id' in song['tracks'][0] ):
+				details = song['audio_summary']
+				details.pop('analysis_url')
+				details.pop('audio_md5')
+				for key in ['song_hotttnesss','artist_name','title']:
+					details[key] = song[key]
+				details['artist_id'] = artist
+				details['track_id'] = song['tracks'][0]['foreign_id']
+				df = df.append(details, ignore_index=True)
+				print(song['title'],song['artist_name'])
+	df = df.set_index('track_id')
+	if not(reset):
+		df2 = pd.read_csv('song_db.csv', encoding='utf-8')
+		df2 = df2.set_index('track_id')
+		df = df2.append(df)
+	df.to_csv('song_db.csv', encoding='utf-8')
